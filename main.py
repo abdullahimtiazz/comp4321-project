47a39afac805b6b266daa17c0faf56671286288e
abdullahimtiazz <iabdullah@connect.ust.hk>
2025-03-23 19:57:46 +0800

    added comments and edited readme.md

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from crawler import Crawler
 
-crawler = Crawler(start_url= "https://comp4321-hkust.github.io/testpages/testpage.htm", max_pages= 30)
-crawler.crawl()
-crawler.generate_spider_result()
-crawler.index.close()
+crawler = Crawler(start_url= "https://comp4321-hkust.github.io/testpages/testpage.htm", max_pages= 30) 
+# you may choose to change the start_url to another url for testing, and max_pages to a different number.
+
+crawler.crawl() # start crawling
+crawler.generate_spider_result()    # output the contents in the database to spider_result.txt in the format specified in README.md
+crawler.index.close()   # gracefully close the database connection
